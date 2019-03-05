cfae3e709b0c899582bb480171db4cc289facd64
Pathan <abbas.pathan@capgemini.com>
2019-03-05 10:46:16 +0530

    Added Multiplication() function

--- calculator.java
+++ calculator.java
@@ -11,4 +11,7 @@ public class calculator{
 	public static void sub(int no1,int no2){
 		System.out.println("Substraction="+(no1-no2));
 	}
+	public static void mul(int no1,int no2){
+		System.out.println("Multiplication="+(no1*no2));
+	}
 }
